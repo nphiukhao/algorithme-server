10f8c0f5ae2a16895c73cbbf90817fda0a2131fd
nphiukhao <nphiukhao@gmail.com>
2020-01-08 20:23:32 -0800

    changed DB_URL to DATABASE_URL

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'https://algorithme.now.sh',
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin:asdf@localhost/spaced-rep',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin:asdf@localhost/spaced-rep',
   JWT_SECRET:  process.env.JWT_SECRET || 'spaced-rep-jwt-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
