ba2e50c12ba4dfc72c72b57fe63cdc78266f052f
nphiukhao <nphiukhao@gmail.com>
2020-01-08 21:00:03 -0800

    testing

--- src/config.js
+++ src/config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'https://algorithme.now.sh',
+  CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin:asdf@localhost/spaced-rep',
   JWT_SECRET:  process.env.JWT_SECRET || 'spaced-rep-jwt-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
+//'https://algorithme.now.sh'
+
+//http://localhost:3000/
